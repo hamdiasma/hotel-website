685b1e2c18a46a2e928f56227bf1077f6343cbc6
hamdiasma <hamdi.babdelhafidh@gmail.com>
2020-07-22 14:23:31 +0100

    intial create pages

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return <div>Hello Error page</div>;
+};
+
+export default ErrorPage;
---
+++ src/pages/SingleRoom.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingleRoom = () => {
+    return (
+        <div>
+            hello from single Romm
+        </div>
+    )
+}
+
+export default SingleRoom
