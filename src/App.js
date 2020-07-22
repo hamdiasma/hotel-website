70b21cb0f86c50e2b097a09c1735284aeff53410
hamdiasma <hamdi.babdelhafidh@gmail.com>
2020-07-22 14:40:46 +0100

    add router to the app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
-  return <div className="App">Learn React</div>;
+  return (
+    <div className="App container">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:single" component={SingleRoom} />
+        <Route component={ErrorPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
